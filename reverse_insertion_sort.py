bf05bac99e9a3a0658754ccdbba0a90a726e9f03
Eric Gomes <ericsilva@meucredere.com.br>
2021-05-02 19:08:22 -0300

    Criado insetoin_sorte reversa

--- reverse_insertion_sort.py
+++ reverse_insertion_sort.py
@@ -1,9 +1,12 @@
-# Algoritmo de Ordenação por Inserção.
-def insertion_sort(array: list) -> list:
+'''
+O desafio aqui é recriar o algorítmo de inserção ordenando
+do maior para o menor.
+'''
+def reverse_insertion_sort(array: list) -> list:
     for element in range(1, len(array)):
         key = array[element]
         i = element - 1
-        while i >= 0 and array[i] > key:
+        while i >= 0 and array[i] < key:
             array[i + 1] = array[i]
             i -= 1
         array[i + 1] = key
@@ -13,5 +16,5 @@ def insertion_sort(array: list) -> list:
 object_array = input("Informe um array(lista) a ser ordenada. A lista deve ser de Inteiros: ").split(",")
 array = []
 array = list(map(lambda x: int(x.strip()), object_array))
-ordered_array = insertion_sort(array)
+ordered_array = reverse_insertion_sort(array)
 print(ordered_array)
